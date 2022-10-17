aeb6d721e738390683ce11ca950f09832508cee0
cayson <buffalo142001@gmail.com>
2022-10-17 20:08:19 +0000

    Updated files

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const https = require('https');
+const fs = require('fs')
 const {v4: uuidv4} = require('uuid');
-const port = 3000;
+const port = 443
 const app = express();
 const {createClient} = require('redis');
 const md5 = require('md5');
@@ -13,10 +15,22 @@ const redisClient = createClient(
 
 app.use(bodyParser.json());
 
-app.listen(port, async ()=> {
+https.createServer({
+
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert'),
+    ca: fs.readFileSync('chain.pem')
+    // passphrase: 'P@ssw0rd'
+
+}, app).listen(port, async () => {
     await redisClient.connect();
-    console.log('listening on port '+port);
+    console.log('Listening...')
 });
+
+// app.listen(port, async ()=> {
+//     await redisClient.connect();
+//     console.log('listening on port '+port);
+// });
 
 app.get('/', (req,res)=>{
     res.send('Hello World!')
@@ -26,23 +40,34 @@ app.use(express.static('public'));
 
 app.post('/user', (req , res) => {
     const newUserRequestObject = req.body;
-    console.log('New User:',JSON.stringify(newUserRequestObject))
-    redisClient.hSet('users', req.body.email,JSON.stringify(newUserRequestObject))
-    res.send('New user added!')
+    console.log('New User:',JSON.stringify(newUserRequestObject));
+    const hashPassword = md5(req.body.password);
+    newUserRequestObject.password = hashPassword;
+    newUserRequestObject.verifyPassword = hashPassword;
+    redisClient.hSet('users', req.body.email,JSON.stringify(newUserRequestObject));
+    res.send('New user created');
 });
 
-app.post("/login", (req , res) =>{
+app.post("/login", async (req , res) =>{
     const loginEmail = req.body.userName;
     console.log(JSON.stringify(req.body));
     console.log('loginEmail', loginEmail);
-    const loginPassword = req.body.password;
-    console.log("loginPassword", loginPassword)
+    const loginPassword = md5(req.body.password);
+    console.log("loginPassword", loginPassword);
     // res.send("Who are you!?")
 
-    if (loginEmail == "test2045@testy.edu" && loginPassword == "12345@Ab"){
+    const userString=await redisClient.hGet('users',loginEmail);
+    const userObject=JSON.parse(userString);
+
+    if(userString=='' || userString ==null){
+        res.status(404);
+        res.send('User not found');
+    }
+    else if (loginEmail == userObject.userName && loginPassword == userObject.password){
         const token = uuidv4();
         res.send(token);
-    } else{
+    }
+    else{
         res.status(401);//unauthorized
         res.send("Invalid user or password");
     }
